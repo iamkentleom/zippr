611e563de47e0d2aef0b80ffa9468a02984bba49
iamkentleom <leomakibulan@gmail.com>
2020-05-15 11:49:19 +0800

    refactor(init): default to zippr.yaml file
    - uses yaml instead of .zippr file
    - includes comments in the initial file generated
    - matches all the files in the directory by default

--- commands/init.js
+++ commands/init.js
@@ -1,18 +1,19 @@
+const { green } = require('chalk')
+const { normalize } = require('path')
 const { writeFile } = require('fs')
 
-const content = `[
-    {
-        "output": "Release",
-        "extension": "zip",
-        "include": [
-            "*"
-        ]
-    }
-]`
+const content = `---
+output: release # output filename
+extension: zip # zip or tar
+include: # files to be included
+    # uses glob pattern as defined in https://github.com/isaacs/node-glob#glob-primer
+    - "**"`
 
 const init = () => {
-    writeFile('.zippr', content, err => {
+    writeFile('zippr.yaml', content, err => {
         if(err) throw err
+        const file = normalize(process.cwd() + '/zippr.yaml')
+        console.log(green(`Initialized zippr process in ${ file }`))
     })
 }
 
